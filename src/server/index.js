80f243dd470d9701e57192aca9404d319e6d25b0
Grégory <gregory.latinier@gmail.com>
2018-07-02 17:21:35 +0200

    Init starter app

---
+++ src/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('dist'));
+app.get('/api/heartbeat', (req, res) => res.send({ success: true }));
+app.listen(8080, () => console.log('Listening on port 8080!'));
